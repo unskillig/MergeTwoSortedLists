3665cba52f18d7869feb020438cbfcac84b05467
nils_hackstein <nils.hackstein2@gmail.com>
2020-03-01 15:17:11 +0100

    Basic logic implemented, but next-nodes in merged List have to be set to null after insertion

--- src/Run.java
+++ src/Run.java
@@ -16,31 +16,64 @@ public class Run {
 		liste2.addNodeTail(new Node(10));
 		liste2.print();
 		
+		CustomLinkedList mergedList = mergeTwoSortedLists(liste1, liste2);
+		mergedList.print();
+		
+		
+		
 
 	}
 	
-	public void mergeTwoSortedLists(CustomLinkedList liste1, CustomLinkedList liste2) {
+	public static CustomLinkedList mergeTwoSortedLists(CustomLinkedList liste1, CustomLinkedList liste2) {
 		CustomLinkedList mergedList = new CustomLinkedList();
 		Node iteratorMergedList = mergedList.getHead();
 		Node iteratorListe1 = liste1.getHead();
 		Node iteratorListe2 = liste2.getHead();
 		
-		while(iteratorListe1 != null || iteratorListe2 != null) {
-			if(iteratorListe1 != null && iteratorListe2 != null) {
-				if(iteratorListe1.getValue() < iteratorListe2.getValue()) {
-					if(mergedList.getHead() == null) {
-						mergedList.setHead(iteratorListe1);
-						mergedList.setTail(iteratorListe1);
-						iteratorMergedList = iteratorListe1;
-					}
-					else {
-					
-					}
-					
-				}
+		// erste Node in mergedList setzen
+		if(iteratorListe1 != null & iteratorListe2 != null) {
+			if(iteratorListe1.getValue() > iteratorListe2.getValue()) {
+				mergedList.setHead(iteratorListe2);
+				mergedList.setTail(iteratorListe2);
+				iteratorMergedList = iteratorListe2;
+				iteratorListe2 = iteratorListe2.getNext();
+			}
+			else {
+				mergedList.setHead(iteratorListe1);
+				mergedList.setTail(iteratorListe1);
+				iteratorMergedList = iteratorListe1;
+				iteratorListe1 = iteratorListe1.getNext();
 			}
 		}
+		else {
+			System.out.println("Beide Listen weisen keine Nodes auf.");
+			return null;
+		}
+		
+		
+		while(iteratorListe1 != null || iteratorListe2 != null) {
+			
+			if(iteratorListe1 != null && iteratorListe2 != null) {
+				if(iteratorListe1.getValue() > iteratorListe2.getValue()) {
+					iteratorMergedList.setNext(iteratorListe2);
+				}
+				else {
+					iteratorMergedList.setNext(iteratorListe1);
+				}
+			}
+			else if(iteratorListe1 != null) {
+				iteratorMergedList.setNext(iteratorListe1);
+			}
+			
+			else if(iteratorListe2 != null) {
+				iteratorMergedList.setNext(iteratorListe2);
+			}
+			
+			iteratorMergedList = iteratorMergedList.getNext();
+			mergedList.setTail(iteratorMergedList.getNext());
+		}
+		
+		return mergedList;
 	}
 	
-
 }
